3bf001d6a3731e03eaab0ef26207b7ae7d810312
tryingtokeepup <kl.hawaii@gmail.com>
2019-01-02 11:39:30 -0800

    oh no, got lost in my code for a second. THIS is the first commit of the day.

---
+++ pair-me-client/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+          <p>
+            Pair-Me, the revolutionary way to get people ... paired up for coding.
+          </p>
+          <p>Whooooooo.</p>
+          <a
+            className="App-link"
+            href="https://google.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Not sure what style we will use, but let's keep this styling for now. 
+          </a>
+
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
