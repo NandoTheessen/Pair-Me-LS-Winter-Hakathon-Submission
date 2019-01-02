7bf438a1ff0794e36f7f048ca4622194ec77de2f
tryingtokeepup <kl.hawaii@gmail.com>
2019-01-02 11:47:26 -0800

    poopie.

--- pair-me-client/src/index.js
+++ pair-me-client/src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import {BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore( {},
+     
+    applyMiddleware(thunk, logger)
+  );
+
+
+ReactDOM.render(<Provider store={store}> <Router><App/></Router></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
