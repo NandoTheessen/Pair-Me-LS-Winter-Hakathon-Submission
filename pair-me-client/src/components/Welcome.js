829cb406f059797f0001eb1e4341756db25649a6
iAmAdamReid <adam@adamreid.me>
2019-01-03 11:35:35 -0800

    Add PairMe web copy to welcome page, still needs styling

--- pair-me-client/src/components/Welcome.js
+++ pair-me-client/src/components/Welcome.js
@@ -7,10 +7,57 @@ import Login from './Login'
 const Welcome = props => {
   return (
     <div className="welcome-container">
-      <div className="welcome-header">Welcome to PairMe!</div>
-      To get started, login here: Need an account?{' '}
-      <NavLink to="/register">Sign up here!</NavLink>
+
+    <div className = 'welcome-banner'>
+    <h1>PairMe</h1>
+    <h2>Get help when you need it, give help when you can.</h2>
+    {/* Full-width banner image with logo
+    Nav stays sticky on top with sign-up button */}
     </div>
+
+
+    <div className="welcome-header">
+    <h3>Welcome to PairMe! We're building a better way for students to find pair-programming partners. </h3>
+    </div>
+        <div className = 'welcome-content'>
+<p>As developers, we understand that feeling lost or needing guidance is an integral part of the development process. Sometimes you need to leverage other people’s eyes, ears, and brains to get you out of a coding funk. But it’s not always easy to find the help you need, when you need it. 
+</p>
+<p>Our mission is to provide a visual dashboard that allows students to request and offer help on challenges that arise in the curriculum, as well as seek guidance on individual projects. Through the usage of signaling “beacons”, we can automatically pair those seeking guidance with those offering assistance. The process is simple:
+</p>
+<p>
+    Say you’re being introduced to React for the first time. You’ve watched the lecture video, tried some online resources, and put the StackOverflow servers under extreme load trying to understand lifecycle methods. It feels like you’re slowly going crazy, stuck in isolation, and just want someone to talk this through with you.
+
+</p>
+<p>
+    Another student finished learning React a few weeks ago, and loves to bring other fledgling developers into the light of component-based architecture. They light a beacon offering help with React and other JavaScript topics.
+
+</p>    
+<p>
+    Once you light your beacon asking for help through the JSX jungle, a Slack notification informs you of a Zoom meeting that’s been set up automatically where you can talk to the other student about all things React. 
+
+</p>    
+<p>
+    And get this: your meeting will be listed on the PairMe dashboard in case other students want to join in on the discussion (unless you signal for a private meeting, of course ;) ).
+
+</p>    
+<p>
+    We automate the process of finding face-to-face code instruction so that your calls for help don’t get lost in the Slack stampede. And as for helpers, no good deed goes unrewarded! We’ll keep track of how many debug dragons you’ve slain, so you can show off to all the other devs your skills in collaborative problem solving.
+
+</p>    
+<p>
+    PairMe also reduces redundancy, by letting students know when a meeting on a topic is active, so they can jump right in without having to look for the link, or start another conference.
+
+</p>    
+<p>
+    Coding is fun. It’s challenging. Sometimes, it’s really hard, but it’s always a little easier when we work together. 
+
+</p>    
+<p>
+    PairMe puts programmers together, because more importantly than learning to code, are the friends we make along the way.
+
+</p>        
+</div>
+</div>
   )
 }
 
