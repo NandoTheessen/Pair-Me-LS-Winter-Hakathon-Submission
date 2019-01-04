23fe855bb8ba37726d187b61f2f6ed485a35a27c
iAmAdamReid <adam@adamreid.me>
2019-01-04 08:43:25 -0800

    Implements username local storage, attempts redirect, and pulls the teacher/student queues (hopefully) into state

--- pair-me-client/src/actions/index.js
+++ pair-me-client/src/actions/index.js
@@ -9,11 +9,18 @@ export const LOGGED_OUT = 'LOGGED_OUT'
 export const LOGGED_IN = 'LOGGED_IN'
 export const LOGGING_IN = 'LOGGING_IN'
 
+
+export const STORING_QUEUES = 'STORING_QUEUES';
+export const QUEUES_STORED = 'QUEUES_STORED';
+
 // Register Dispatches
 export const REGISTER = 'REGISTER'
 
 export const logout = () => {
   return dispatch => {
+    localStorage.removeItem('username');
+    localStorage.removeItem('token');
+    console.log('LOGGED OUT')
     dispatch({ type: LOGGED_OUT })
   }
 }
@@ -22,5 +29,21 @@ export const login = ({ email, access_token, name }) => {
   return dispatch => {
     localStorage.setItem('access_token', access_token)
     dispatch({ type: LOGGED_IN, payload: { email, access_token, name } })
+  }
+}
+
+export const storeLocally = (data) => {
+  return dispatch => {
+    // store the current user
+    dispatch({type: STORING_QUEUES})
+    localStorage.setItem('username', data.data.user.name);
+    localStorage.setItem('token', data.data.access_token);
+    console.log(data.data.access_token);
+    if(data.s_queue && data.t_queue){
+      dispatch({type: QUEUES_STORED, payload: {student: data.s_queue, teacher: data.t_queue, username: data.data.username}})
+    } else {
+      dispatch({type: ERROR})
+    }
+
   }
 }
--- pair-me-client/src/reducers/userReducer.js
+++ pair-me-client/src/reducers/userReducer.js
@@ -1,11 +1,16 @@
-import { LOGGED_IN, LOGGED_OUT } from '../actions/index'
+import { LOGGED_IN, LOGGED_OUT, STORING_QUEUES, QUEUES_STORED } from '../actions/index'
 
 const initialState = {
   email: null,
   access_token: null,
   name: null,
   isLoggedIn: false,
-  error: null
+  error: null,
+  teachers: [],
+  students: [],
+  username: '',
+  storing_queues: false,
+  queues_stored: false,
 }
 
 export const userReducer = (state = initialState, action) => {
@@ -14,8 +19,25 @@ export const userReducer = (state = initialState, action) => {
       const newState = { ...state, ...action.payload }
       newState.isLoggedIn = true
       return newState
+
     case LOGGED_OUT:
       return initialState
+
+    case STORING_QUEUES:
+      return Object.assign({}, state, {
+        storing_queues: true,
+        queues_stored: false,
+      })
+
+    case QUEUES_STORED:
+      return Object.assign({}, state, {
+        storing_queues: false,
+        teachers: action.payload.teachers,
+        students: action.payload.students,
+        username: action.payload.username,
+        queues_stored: true
+      })
+
     default:
       return state
   }
